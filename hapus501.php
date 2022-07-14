431e186b16648a0ed26f4f1846576d198cf68309
gcpamujigit <gcpamujigit@users.noreply.github.com>
2022-07-14 11:11:19 +0700

    Update hapus501.php
    menyesuaikan file hapus501

--- hapus501.php
+++ hapus501.php
@@ -3,9 +3,11 @@ include 'koneksidb301.php';
 
 // menyimpan data id kedalam variabel
 $kodebarang   = $_GET['kodebarang'];
+
 // query SQL untuk delete data
 $query="DELETE from barang where kodebarang='$kodebarang'";
-mysqli_query($koneksi, $query);
+mysqli_query($conn, $query);
+
 // mengalihkan ke halaman tabel
 header("location:pertemuan301db.php");
 ?>
